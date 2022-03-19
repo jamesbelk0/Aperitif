26f180f434a87733ae4e62fc892366541a0a0e7c
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-03-19 14:49:43 -0400

    functioning modal on click via step one button in nav bar

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,12 @@
-myStorage = window.localStorage;
+//modal click to open
+const steponeButton = document.querySelector('#stepOne');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+steponeButton.addEventListener('click', () => {
+  modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+  modal.classList.remove('is-active');
+});
