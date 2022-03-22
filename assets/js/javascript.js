f49c486ec409da1ee26ab6688f20be39ff5e9d07
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-03-22 19:49:53 -0400

    fun stuff

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -13,34 +13,33 @@ modalBg.addEventListener('click', () => {
 });
 
 //modal 'save changes' click
-document.querySelector('.is-success').addEventListener("click", () => getBrew(), modal.classList.remove('is-active'));
+document.querySelector('.button').addEventListener("click", modal.classList.remove('is-active'));
 
 //user input variables
 var userFormEl = document.querySelector("#cityname");
-var cityInputEl = document.querySelector("#cityname");
+var cityInputEl = document.querySelector("#submituser");
 var brewCardEl = document.querySelector("#repos-container");
 
 var formSubmitHandler = function (event) {
   //Prevent page from refreshing
   event.preventDefault();
   //get value from input (cityinput)
-  var postalCode = cityInputEl.value.trim();
+  var userInput = cityInputEl.value.trim();
 
-  if (postalCode) {
-    getBrew(postalCode);
-
+  var getBrew = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.openbrewerydb.org/breweries?by_city=" + userInput;
+  console.log(apiUrl);
+``
+  if (userInput) {
+    getBrew();
     //clear old content
     brewCardEl.textcontent = '';
     cityInputEl.value = '';
   } else {
-    alert("Please enter a zipcode");
+    alert("Please enter a city name");
   }
-};
-//fetch api (openbrewDb)
-var getBrew = function (user) {
-  // format the github api url
-  var apiUrl = "https://api.openbrewerydb.org/breweriesbreweries/search?query=" + userFormEl + "/";
-  
+
   // request to the url
   fetch(apiUrl)
     .then(function (response) {
@@ -51,6 +50,5 @@ var getBrew = function (user) {
       }
     })
 }
-
-userFormEl.addEventListener("submit", formSubmitHandler);
-
+};
+userFormEl.addEventListener("#submituser", formSubmitHandler);
