337f741ffb2d7f3325b112d0329f82e1ce5b508b
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-03-28 10:03:59 -0400

    got fetch request to work and populate on the page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -15,35 +15,50 @@ modalBg.addEventListener('click', () => {
 //modal 'save changes' click
 document.querySelector('.button').addEventListener("click", modal.classList.remove('is-active'));
 
-// apiUrl and user input
-const url = "https://api.openbrewerydb.org/breweries?by_postal="
-const user = document.getElementById('zip').value;
-const apiUrl = url + user;
-
-
-getBrew = function getBrew() {
-  //fetch on click
-  $("#submituser").click(function () {
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => console.log(data));
-
-    // Defining async function
-    async function getBrew(apiUrl) {
-      // Storing response
-      const response = await fetch(url);
-      // Storing data in form of JSON
-      const data = await response.json();
-      console.log(data);
-      $("#submituser").submit(function (e) {
-        e.preventDefault();
-      });
-
-      localStorage.getBrew('name', 'phone', 'street', 'website');
-    };
-  });
+// user input
+function getInputValue() {
+  const user = document.getElementById('myInput').value;
+  e.preventDefault();
+  console.log(user);
 };
+
+//fetch on click
+
+$("#apiUrl").submit(function (e) {
+  e.preventDefault();
+
+  const url = "https://api.openbrewerydb.org/breweries?by_postal="
+  const user = document.getElementById('myInput').value;
+  const apiUrl = url + user;
+  console.log(apiUrl);
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const cards = $('#cards');
+      // for loop for multiple cards
+      for (let i = 0; i < data.length; i++) {
+        var card = data[i]
+        cards.append(`<div id="location" class="column is-one-half">
+    
+        <div id="repos-container" class="card"></div>
+        <h3 class="name">${card.name} <button class="button favorite">Favorite</button></h3>
+        <ul class="pullData">
+          <li id="street">${card.street}</li>
+          <li class="city">${card.city}</li>
+          <li class="phone">${card.phone}</li>
+          <li class="website"><a href="${card.website_url}">${card.website_url}</a></li>
+        </ul>
+      </div>
+      `)
+      }
+    })
+
+      .catch ((error) => {
+        console.error(error);
+      });
+});
+
+//     localStorage.setItem(apiUrl, JSON.stringify(favorite));
+//   };
+// });
 console.log(localStorage);
-
-// Calling that async function
-
